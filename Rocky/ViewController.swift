3086c99f09f917ea0fb3f064bc89c43f577c8791
teresa kim <teresakim@teresas-macbook-air.local>
2019-02-06 21:12:32 -0600

    Initial Commit

---
+++ Rocky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rocky
+//
+//  Created by teresa kim on 2/6/19.
+//  Copyright © 2019 teresa kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
