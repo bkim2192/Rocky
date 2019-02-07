15262370040d6e7ea8bbab2b373f6f8643ef747e
teresa kim <teresakim@teresas-macbook-air.local>
2019-02-06 21:54:47 -0600

    COmmited

--- Rocky/ViewController.swift
+++ Rocky/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yourChoice: UIImageView!
+    @IBOutlet weak var computerChoice: UIImageView!
+    @IBOutlet var Pics: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let array = [1,2,3]
+        //if let is for "if a variable has a value, do the first line of code, and if the variable doesnt have a value, do the second line of code. Ex. if let num = randomNumber {This is the part if it has a value}
+        var randomNumber = array.randomElement()
+        var number = randomNumber!
+        if number == 1{
+            computerChoice.image = UIImage(named:"Rock")
+        }; if number == 2{
+            computerChoice.image = UIImage(named: "Paper")
+        }; if number == 3 {
+            computerChoice.image = UIImage(named:"Scissors")
+        }
+        
+    
+    }
+    
+    @IBAction func Pictures(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
 }
 
